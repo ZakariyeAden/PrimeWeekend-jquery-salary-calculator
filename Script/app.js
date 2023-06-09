29ccd32a9ded3969eb0a669330e9e103045d5c8a
ZakariyeAden <zakariye5417@gmail.com>
2023-06-09 16:37:28 -0500

    writing pseudo and html elements

---
+++ Script/app.js
@@ -0,0 +1,24 @@
+
+$(document).ready(getStarted);
+
+function getStarted(){
+  console.log('Get started');
+}
+// ***********************************************************
+// ****************** PLAN/PSEUDO CODE ******************** //
+// ***********************************************************
+// Add in the HTML Elements and format.
+// Add ID for the Inputs and submit
+/* Have a function to for adding all of the inputs */
+  // Get all the value for the first name,last name, id, title, and annual salary
+  // Console log all the data
+  // Dispaly and append the data in a table
+  // Empty the values after submited
+  // Start with global for the total Monthly and divide by 12
+  // Add up all the data for Annual salary and / 12.May need to figure it out by adding in the function or ready now function ?
+  // Console log the total Monthy varable
+/* Have a function to delete the employees */
+  // First console log if the button is being clicked
+  // Have a delegation to target the tbody 
+  // Target the parents to delete them all.
+// * Once everthing works style it to make it look nice!
